745a7e761a46ba9d9084fee72cf43ab9ee0d5e35
madrigalsalazar <diego.madrigalsalazar@ucr.ac.cr>
2022-10-11 22:03:36 -0600

    c

--- server.py
+++ server.py
@@ -31,7 +31,7 @@ def main():
         #os.system('echo 1234 | sudo openssl ca -config /home/reich/root/ca/issuing_ca/openssl.cnf -engine pkcs11 -keyform engine -keyfile 02 -extensions v3_ca -days 365 -notext -md sha256 -in csr.csr -out /home/reich/root/ca/issuing_ca/certs/prueba.pem')   
         #os.system("2728")
 
-        p = Popen('sudo openssl ca -config /home/reich/root/ca/issuing_ca/openssl.cnf -engine pkcs11 -keyform engine -keyfile 02 -extensions v3_ca -days 365 -notext -md sha256 -in csr.csr -out /home/reich/root/ca/issuing_ca/certs/prueba.pem', shell=True, stdin=PIPE)
+        p = Popen('sudo openssl ca -config /home/reich/root/ca/issuing_ca/openssl.cnf -engine pkcs11 -keyform engine -keyfile 02 -extensions v3_ca -days 365 -notext -md sha256 -in csr.csr -out /home/reich/root/ca/issuing_ca/certs/prueba.pem', stdout=PIPE, stdin=PIPE, stderr=PIPE)
         for x in range(100):
             print("Hola")        
         p.communicate(input='1234'.encode())
