af303311cbb72ead2326bf111d1843cf03822f9f
madrigalsalazar <diego.madrigalsalazar@ucr.ac.cr>
2022-10-11 21:44:32 -0600

    c

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import socket
-from os import system 
+import os 
 import subprocess
 
 PORT = 8800
@@ -22,10 +22,10 @@ def main():
         #foo_proc = subprocess.Popen(['sudo openssl ca -config /home/reich/root/ca/issuing_ca/openssl.cnf -engine pkcs11 -keyform engine -keyfile 02 -extensions v3_ca -days 365 -notext -md sha256 -in csr.csr -out /home/reich/root/ca/issuing_ca/certs/prueba.pem'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
         #foo_proc.stdin.write(b"1234")
 
-        subprocess.run('sudo openssl ca -config /home/reich/root/ca/issuing_ca/openssl.cnf -engine pkcs11 -keyform engine -keyfile 02 -extensions v3_ca -days 365 -notext -md sha256 -in csr.csr -out /home/reich/root/ca/issuing_ca/certs/prueba.pem', capture_output=True, text=True, input="y")
+        #subprocess.run('sudo openssl ca -config /home/reich/root/ca/issuing_ca/openssl.cnf -engine pkcs11 -keyform engine -keyfile 02 -extensions v3_ca -days 365 -notext -md sha256 -in csr.csr -out /home/reich/root/ca/issuing_ca/certs/prueba.pem', capture_output=True, text=True, input="y")
         #subprocess.run([""], capture_output=True, text=True, input="1234")
 
-        #os.system()   
+        os.system('echo 1234 | sudo openssl ca -config /home/reich/root/ca/issuing_ca/openssl.cnf -engine pkcs11 -keyform engine -keyfile 02 -extensions v3_ca -days 365 -notext -md sha256 -in csr.csr -out /home/reich/root/ca/issuing_ca/certs/prueba.pem')   
 
         #os.system("2728")
 
