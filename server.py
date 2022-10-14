1ac5d104be300e7235d1dc04a2704a48e2dc6d58
madrigalsalazar <diego.madrigalsalazar@ucr.ac.cr>
2022-10-13 22:30:40 -0600

    c

--- server.py
+++ server.py
@@ -14,19 +14,19 @@ def main():
         connection, direcction = sock.accept()                     
         entity_csr = connection.recv(2048)
         request = crypto.load_certificate_request(crypto.FILETYPE_PEM, entity_csr)  
-        common_name = request.get_subject().commonName.strip()      
+        common_name = request.get_subject().commonName              
         
-        csr_file = open(common_name + '.csr', 'wb')
+        csr_file = open(common_name.strip() + '.csr', 'wb')
         csr_file.write(entity_csr)
         csr_file.close()  
         
         command = 'echo 1234 | sudo -S openssl ca -config /home/reich/root/ca/issuing_ca/openssl.cnf -batch \
         -engine pkcs11 -keyform engine -keyfile 02 -extensions v3_ca -days 365 -notext -md sha256 -passin pass:1234 \
-        -in ' + common_name + '.csr -out /home/reich/root/ca/issuing_ca/certs/' + common_name + '.pem'
+        -in ' + common_name.strip() + '.csr -out /home/reich/root/ca/issuing_ca/certs/' + common_name.strip() + '.pem'
 
         os.system(command) 
 
-        crt_file = open('/home/reich/root/ca/issuing_ca/certs/' + common_name + '.pem', 'rb')         
+        crt_file = open('/home/reich/root/ca/issuing_ca/certs/' + common_name.strip() + '.pem', 'rb')         
         connection.sendall(crt_file.read())      
         crt_file.close()
         
