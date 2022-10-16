35bb454dbfa901460328442adb984f6fcce2d114
madrigalsalazar <diego.madrigalsalazar@ucr.ac.cr>
2022-10-15 22:44:07 -0600

    changes

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
+from asyncio.log import logger
 import datetime, socket, os
 from OpenSSL import crypto
 
 PORT = 9225
 HOST = ''
-HOME = os.getenv("HOME")
 
 def save_csr(csr, entity_name):
     csr_file = open('/etc/pki/ca/issuing_ca/csr/' + entity_name + '.csr', 'wb')
@@ -22,20 +22,24 @@ def main():
     sock.bind((HOST, PORT))
     sock.listen(2)       
 
-    while True:                 
-        conn, dir = sock.accept()
+    while True:
+        try:                
+            conn, dir = sock.accept()
 
-        entity_csr = conn.recv(4096)
-        csr = crypto.load_certificate_request(crypto.FILETYPE_PEM, entity_csr)  
-        entity_name = csr.get_subject().commonName.replace(" ", "")       
-        save_csr(entity_csr, entity_name)
-        sign_csr(entity_name)      
+            entity_csr = conn.recv(4096)
+            csr = crypto.load_certificate_request(crypto.FILETYPE_PEM, entity_csr)  
+            entity_name = csr.get_subject().commonName.replace(" ", "").lower()       
+            save_csr(entity_csr, entity_name)
+            sign_csr(entity_name)      
 
-        crt_file = open('/etc/pki/ca/issuing_ca/certs/' + entity_name + '.pem', 'rb')         
-        conn.sendall(crt_file.read())      
-        crt_file.close()
-        
-        conn.close()        
+            crt_file = open('/etc/pki/ca/issuing_ca/certs/' + entity_name + '.pem', 'rb')         
+            conn.sendall(crt_file.read())      
+            crt_file.close()
+            
+            conn.close()
+        except BaseException as exception:
+            logger.error('Sorry, there is an exception: '+ str(exception))
+            break        
 
 if __name__ == "__main__":
     main()
