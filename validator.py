f164ebc937dd793c4184ae1d063f4e2f4c863527
madrigalsalazar <diego.madrigalsalazar@ucr.ac.cr>
2022-10-18 17:23:14 -0600

    validation commit

---
+++ validator.py
@@ -0,0 +1,9 @@
+import sys
+from certvalidator import CertificateValidator, ValidationContext
+
+with open(sys.argv[1], 'rb') as f:
+    end_entity_cert = f.read()
+
+context = ValidationContext(allow_fetching=True)
+validator = CertificateValidator(end_entity_cert, validation_context=context)
+
