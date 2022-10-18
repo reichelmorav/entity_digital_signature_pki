c736954d4d5b611421c1e7ec20d4a5439d89371d
madrigalsalazar <diego.madrigalsalazar@ucr.ac.cr>
2022-10-18 17:55:32 -0600

    c

--- validator.py
+++ validator.py
@@ -1,8 +1,10 @@
 import sys
 from certvalidator import CertificateValidator, ValidationContext
+from numpy import r_
 
 with open(sys.argv[1], 'rb') as f:
     end_entity_cert = f.read()
+    print(end_entity_cert)
 
 context = ValidationContext(allow_fetching=True)
 validator = CertificateValidator(end_entity_cert, validation_context=context)
