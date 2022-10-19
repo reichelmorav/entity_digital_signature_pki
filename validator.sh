db1ae33adf27d194ed14c14b22527baa16c60031
madrigalsalazar <diego.madrigalsalazar@ucr.ac.cr>
2022-10-18 22:14:25 -0600

    c

--- validator.sh
+++ validator.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-crl_URL="$(openssl asn1parse -in /etc/pki/entities_issued/facultaddeartes/facultaddeartes.pem | grep -A 1 'X509v3 CRL Distribution Points' | tail -1 | cut -d: -f 4 | cut -b21- | perl -ne 's/(..)/print chr(hex($1))/ge; END {print "\n"}')"
+crl_URL="$(openssl asn1parse -in $1 | grep -A 1 'X509v3 CRL Distribution Points' | tail -1 | cut -d: -f 4 | cut -b21- | perl -ne 's/(..)/print chr(hex($1))/ge; END {print "\n"}')"
 echo "$crl_URL"
 wget -P /etc/pki/crls/ "$crl_URL"
