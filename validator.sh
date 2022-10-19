32f0ee52e6a53e38a6a6c21d3ef7f86f804e21d4
madrigalsalazar <diego.madrigalsalazar@ucr.ac.cr>
2022-10-18 22:12:02 -0600

    c

--- validator.sh
+++ validator.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-crl_URL = "$(openssl asn1parse -in $1 | 
+crl_URL="$(openssl asn1parse -in $1 | 
 grep -A 1 'X509v3 CRL Distribution Points' | tail -1 | cut -d: -f 4 | cut -b21)"
 
 echo "$crl_URL"
