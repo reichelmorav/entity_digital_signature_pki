1737bb8ee69099996a91a34e26cb640797981b8c
madrigalsalazar <diego.madrigalsalazar@ucr.ac.cr>
2022-10-19 14:58:03 -0600

    changes

--- validator.sh
+++ validator.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cert_serial="$(openssl x509 -noout -serial -in /etc/pki/entities_issued/facultaddeartes/facultaddeartes.pem | cut -d'=' -f2 | sed 's/../&:/g;s/:$//')"
+cert_serial="$(openssl x509 -noout -serial -in /etc/pki/entities_issued/facultaddeartes/facultaddeartes.pem | cut -d'=' -f2)"
 echo "$cert_serial"
 crl_URL="$(openssl asn1parse -in $1 | grep -A 1 'X509v3 CRL Distribution Points' | tail -1 | cut -d: -f 4 | cut -b21- | perl -ne 's/(..)/print chr(hex($1))/ge; END {print "\n"}')"
 echo "$crl_URL"
@@ -7,6 +7,6 @@ wget -P /etc/pki/crls/ "$crl_URL"
 
 openssl crl -in /etc/pki/crls/ca_intermediate_entities_issuing.crl -outform DER -out /etc/pki/crls/issuing.crl
 
-#openssl crl -in ssca-sha2-g6.crl -inform DER -text -noout | grep YOUR_SERIAL_NUMBER
+openssl crl -in ssca-sha2-g6.crl -inform DER -text -noout | grep "$cert_serial"
 
 
