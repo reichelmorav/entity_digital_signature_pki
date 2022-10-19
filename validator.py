dd6878819465a8846f8842a78ff1fee3622d5b13
madrigalsalazar <diego.madrigalsalazar@ucr.ac.cr>
2022-10-18 20:55:11 -0600

    c

--- validator.py
+++ validator.py
@@ -1,4 +1,5 @@
 from cryptography import x509
+from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.hashes import SHA256
 from cryptography.x509 import ocsp
@@ -6,8 +7,9 @@ from cryptography.x509.ocsp import OCSPResponseStatus
 from cryptography.x509.oid import ExtensionOID, AuthorityInformationAccessOID
 import sys
 
-def get_ocsp_server(cert):
-    aia = cert.extensions.get_extension_for_oid(ExtensionOID.AUTHORITY_INFORMATION_ACCESS).value
+def get_ocsp_server(cert1):
+    cert2 = x509.load_pem_x509_certificate(cert1.encode('ascii'), default_backend())
+    aia = cert2.extensions.get_extension_for_oid(ExtensionOID.AUTHORITY_INFORMATION_ACCESS).value
     print(aia)
     ocsps = [ia for ia in aia if ia.access_method == AuthorityInformationAccessOID.OCSP]
     if not ocsps:
