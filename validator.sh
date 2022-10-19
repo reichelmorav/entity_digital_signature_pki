a520a06c1e7cddf0887d050bc84830ef4b68dd4c
madrigalsalazar <diego.madrigalsalazar@ucr.ac.cr>
2022-10-19 14:52:11 -0600

    changes

--- validator.sh
+++ validator.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-cert_serial = openssl x509 -noout -serial -in $1 | cut -d'=' -f2 | sed 's/../&:/g;s/:$//'
+cert_serial = "$(openssl x509 -noout -serial -in $1 | cut -d'=' -f2 | sed 's/../&:/g;s/:$//')"
 echo "$cert_serial"
 crl_URL="$(openssl asn1parse -in $1 | grep -A 1 'X509v3 CRL Distribution Points' | tail -1 | cut -d: -f 4 | cut -b21- | perl -ne 's/(..)/print chr(hex($1))/ge; END {print "\n"}')"
 echo "$crl_URL"
