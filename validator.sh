158dfdec0ed08e17c16afb36513bad1e52a7a67f
madrigalsalazar <diego.madrigalsalazar@ucr.ac.cr>
2022-10-19 14:34:45 -0600

    ca

--- validator.sh
+++ validator.sh
@@ -3,3 +3,5 @@
 crl_URL="$(openssl asn1parse -in $1 | grep -A 1 'X509v3 CRL Distribution Points' | tail -1 | cut -d: -f 4 | cut -b21- | perl -ne 's/(..)/print chr(hex($1))/ge; END {print "\n"}')"
 echo "$crl_URL"
 wget -P /etc/pki/crls/ "$crl_URL"
+
+openssl crl -in /etc/pki/crls/ "$crl_URL" -inform DER -text -noout
