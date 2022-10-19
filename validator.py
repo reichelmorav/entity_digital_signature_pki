9a2c71d9f61150431c32cb6aa0a11d26c384bc79
madrigalsalazar <diego.madrigalsalazar@ucr.ac.cr>
2022-10-18 21:57:55 -0600

    c

--- validator.py
+++ validator.py
@@ -7,9 +7,12 @@ from cryptography.x509.ocsp import OCSPResponseStatus
 from cryptography.x509.oid import ExtensionOID, AuthorityInformationAccessOID
 import sys
 
-def get_ocsp_server(cert):    
-    cert2 = x509.load_pem_x509_certificate(open(cert, 'rb').read(), default_backend())
-    aia = cert2.extensions.get_extension_for_oid(ExtensionOID.CRL_DISTRIBUTION_POINTS).value
-    print(aia[1])     
+def get_crl_server(cert):    
+    certifocate = x509.load_pem_x509_certificate(open(cert, 'rb').read(), default_backend())
+    aia = certifocate.extensions.get_extension_for_oid(ExtensionOID.CRL_DISTRIBUTION_POINTS).value
+    crls = [ia for ia in aia if ia.access_method == CRLEntryExtensionOID.]
+    if not crls:
+        raise Exception(f'no ocsp server entry in AIA')
+    return crls[0].access_location.value         
 
-get_ocsp_server(sys.argv[1])
+print(get_crl_server(sys.argv[1]))
