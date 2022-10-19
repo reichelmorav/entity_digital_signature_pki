5a22fdc2a8d26a42dbc334402c26974635a6478c
madrigalsalazar <diego.madrigalsalazar@ucr.ac.cr>
2022-10-18 20:44:27 -0600

    c

--- validator.py
+++ validator.py
@@ -1,11 +1,17 @@
+from cryptography import x509
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.hashes import SHA256
+from cryptography.x509 import ocsp
+from cryptography.x509.ocsp import OCSPResponseStatus
+from cryptography.x509.oid import ExtensionOID, AuthorityInformationAccessOID
 import sys
-from certvalidator import CertificateValidator, ValidationContext
 
-with open(sys.argv[1], 'rb') as f:
-    end_entity_cert = f.read()
-    print(end_entity_cert)
+def get_ocsp_server(cert):
+    aia = cert.extensions.get_extension_for_oid(ExtensionOID.AUTHORITY_INFORMATION_ACCESS).value
+    print(aia)
+    ocsps = [ia for ia in aia if ia.access_method == AuthorityInformationAccessOID.OCSP]
+    if not ocsps:
+        raise Exception(f'no ocsp server entry in AIA')
+    return ocsps[0].access_location.value
 
-context = ValidationContext(allow_fetching=True)
-validator = CertificateValidator(end_entity_cert, validation_context=context)
-validator.validate_usage("digital_signature")
-
+get_ocsp_server(sys.argv[1])
