0d7ed784c38dfa74ed3b6bb56b23dbe95c7dda93
madrigalsalazar <diego.madrigalsalazar@ucr.ac.cr>
2022-10-21 23:11:27 -0600

    c

--- crl_update.sh
+++ crl_update.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 
+crl_date="$(openssl crl -in /etc/pki/ca/issuing_ca/crl/ca_intermediate_entities_issuing.crl -noout -nextupdate | cut -d'=' -f2)"
+crl_date_f="$(date -d "$crl_date" -u)"
+echo "$crl_date_f"
+
+actual_date="$(date +"%a %b %d %H:%M:%S %p %Z %Y" -u)"
+echo "$actual_date"
+
+if ["$crl_date_f" < "$actual_date"]; 
+then 
+    echo "fecha menor"
+    rm /etc/pki/crl/ca_intermediate_entities_issuing.crl
+    openssl ca -engine pkcs11 -keyform engine -keyfile 02 -gencrl -crldays 30 -cert /etc/pki/ca/issuing_ca/certs/ca_intermediate_issuing.cert.pem -out /etc/pki/ca/issuing_ca/crl/ca_intermediate_entities_issuing.crl
+; else 
+    echo "fecha mayor"; 
+fi
