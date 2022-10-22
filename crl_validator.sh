3598e74eec5f55b17824629ebf3b143c0dbd95ef
madrigalsalazar <diego.madrigalsalazar@ucr.ac.cr>
2022-10-21 21:47:41 -0600

    changes

--- crl_validator.sh
+++ crl_validator.sh
@@ -2,7 +2,8 @@
 
 rm /etc/pki/crls/ca_intermediate_entities_issuing.crl /etc/pki/crls/issuing.crl
 cert_serial="$(openssl x509 -noout -serial -in $1 | cut -d'=' -f2)"
-crl_URL="$(openssl asn1parse -in $1 | grep -A 1 'X509v3 CRL Distribution Points' | tail -1 | cut -d: -f 4 | cut -b21- | perl -ne 's/(..)/print chr(hex($1))/ge; END {print "\n"}')"
+crl_URL="$(openssl asn1parse -in $1 | grep -A 1 'X509v3 CRL Distribution Points' | tail -1 | cut -d: -f 4 | cut -b21- 
+| perl -ne 's/(..)/print chr(hex($1))/ge; END {print "\n"}')"
 
 wget -P /etc/pki/crls/ "$crl_URL" -q
 
@@ -10,4 +11,4 @@ openssl crl -in /etc/pki/crls/ca_intermediate_entities_issuing.crl -outform DER 
 
 status="$(openssl crl -in /etc/pki/crls/issuing.crl -inform DER -text -noout | grep "$cert_serial")"
 
-if [ -z "$status" ]; then echo "Not revoked"; else echo "Revoked"; fi
+if [ -z "$status" ]; then echo "Good"; else echo "Revoked"; fi
