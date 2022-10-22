a0d8dc377c2f5020e7ed9738062dece3c632faf6
madrigalsalazar <diego.madrigalsalazar@ucr.ac.cr>
2022-10-21 23:22:26 -0600

    changes

--- crl_update.sh
+++ crl_update.sh
@@ -7,7 +7,7 @@ echo "$crl_date_f"
 actual_date="$(date +"%a %b %d %H:%M:%S %p %Z %Y" -u)"
 echo "$actual_date"
 
-if ["$crl_date_f" < "$actual_date"]
+if [[ "$crl_date_f" < "$actual_date" ]]
 then 
     echo "fecha menor"
     #rm /etc/pki/ca/issuing_ca/crl/ca_intermediate_entities_issuing.crl
