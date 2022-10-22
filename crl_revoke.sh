fa019e5cc4439fce4ecad5aa5166ab468b68c282
madrigalsalazar <diego.madrigalsalazar@ucr.ac.cr>
2022-10-21 22:19:20 -0600

    ca

--- crl_revoke.sh
+++ crl_revoke.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 openssl ca -config /etc/pki/ca/issuing_ca/openssl.cnf -engine pkcs11 -keyform engine -keyfile 02 -revoke $1
-rm /etc/pki/crls/ca_intermediate_entities_issuing.crl
-openssl ca -engine pkcs11 -keyform engine -keyfile 02 -gencrl -crldays 30 -cert /etc/pki/ca/issuing_ca/certs/ca_intermediate_issuing.cert.pem -out /etc/pki/crls/ca_intermediate_entities_issuing.crl
+rm /etc/pki/crl/ca_intermediate_entities_issuing.crl
+openssl ca -engine pkcs11 -keyform engine -keyfile 02 -gencrl -crldays 30 -cert /etc/pki/ca/issuing_ca/certs/ca_intermediate_issuing.cert.pem -out /etc/pki/ca/issuing_ca/crl/ca_intermediate_entities_issuing.crl
