a7bf347b1691d3a8bfb8f2e66b871f7ebc2ca247
Reichel Mora-Villegas <41088686+reichelmorav@users.noreply.github.com>
2022-10-23 21:53:41 -0600

    Rename oscp_validator.sh to ocsp_validator.sh
    WRONG NAME AGAIN

--- oscp_validator.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ocsp_url="$(openssl x509 -noout -ocsp_uri -in $1)"
-
-openssl ocsp -CAfile /etc/pki/ca/issuing_ca/certs/ca_chain_issuing.cert.pem -url "$ocsp_url" -port 8080 -issuer /etc/pki/ca/issuing_ca/certs/ca_intermediate_issuing.cert.pem -cert $1
