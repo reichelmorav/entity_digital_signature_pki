dacacf5727521846c92c67c732d46d1ac9bbcb97
Reichel Mora-Villegas <41088686+reichelmorav@users.noreply.github.com>
2022-10-23 22:07:59 -0600

    openssl conf added

--- crl_revoke.sh
+++ crl_revoke.sh
@@ -2,4 +2,4 @@
 
 openssl ca -config /etc/pki/ca/issuing_ca/openssl.cnf -engine pkcs11 -keyform engine -keyfile 02 -revoke $1
 rm /etc/pki/ca/issuing_ca/crl/ca_intermediate_entities_issuing.crl
-openssl ca -engine pkcs11 -keyform engine -keyfile 02 -gencrl -crldays 30 -cert /etc/pki/ca/issuing_ca/certs/ca_intermediate_issuing.cert.pem -out /etc/pki/ca/issuing_ca/crl/ca_intermediate_entities_issuing.crl
+openssl ca -config /etc/pki/ca/issuing_ca/openssl.cnf -engine pkcs11 -keyform engine -keyfile 02 -gencrl -crldays 30 -cert /etc/pki/ca/issuing_ca/certs/ca_intermediate_issuing.cert.pem -out /etc/pki/ca/issuing_ca/crl/ca_intermediate_entities_issuing.crl
