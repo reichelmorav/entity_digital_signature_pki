b41c76f164c05749ca74e73fb6f244099dc05e29
madrigalsalazar <diego.madrigalsalazar@ucr.ac.cr>
2022-10-29 15:33:35 -0600

    prueba

--- oscp_validator.py
+++ oscp_validator.py
@@ -1,5 +1,6 @@
 import base64
 import ssl
+from matplotlib.pyplot import get
 import requests
 from urllib.parse import urljoin
 from cryptography import x509
@@ -9,23 +10,6 @@ from cryptography.hazmat.primitives.hashes import SHA256
 from cryptography.x509 import ocsp
 from cryptography.x509.ocsp import OCSPResponseStatus
 from cryptography.x509.oid import ExtensionOID, AuthorityInformationAccessOID
-
-
-def get_cert_for_hostname(hostname, port):
-    conn = ssl.create_connection((hostname, port))
-    context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
-    sock = context.wrap_socket(conn, server_hostname=hostname)
-    certDER = sock.getpeercert(True)
-    certPEM = ssl.DER_cert_to_PEM_cert(certDER)
-    return x509.load_pem_x509_certificate(certPEM.encode('ascii'), default_backend())
-
-
-def get_issuer(cert):
-    aia = cert.extensions.get_extension_for_oid(ExtensionOID.AUTHORITY_INFORMATION_ACCESS).value
-    issuers = [ia for ia in aia if ia.access_method == AuthorityInformationAccessOID.CA_ISSUERS]
-    if not issuers:
-        raise Exception(f'no issuers entry in AIA')
-    return issuers[0].access_location.value
 
 
 def get_ocsp_server(cert):
@@ -42,8 +26,7 @@ def get_issuer_cert(ca_issuer):
         issuerDER = issuer_response.content
         issuerPEM = ssl.DER_cert_to_PEM_cert(issuerDER)
         return x509.load_pem_x509_certificate(issuerPEM.encode('ascii'), default_backend())
-    raise Exception(f'fetching issuer cert  failed with response status: {issuer_response.status_code}')
-
+        raise Exception(f'fetching issuer cert  failed with response status: {issuer_response.status_code}')
 
 def get_oscp_request(ocsp_server, cert, issuer_cert):
     builder = ocsp.OCSPRequestBuilder()
@@ -51,7 +34,6 @@ def get_oscp_request(ocsp_server, cert, issuer_cert):
     req = builder.build()
     req_path = base64.b64encode(req.public_bytes(serialization.Encoding.DER))
     return urljoin(ocsp_server + '/', req_path.decode('ascii'))
-
 
 def get_ocsp_cert_status(ocsp_server, cert, issuer_cert):
     ocsp_resp = requests.get(get_oscp_request(ocsp_server, cert, issuer_cert))
@@ -63,16 +45,20 @@ def get_ocsp_cert_status(ocsp_server, cert, issuer_cert):
             raise Exception(f'decoding ocsp response failed: {ocsp_decoded.response_status}')
     raise Exception(f'fetching ocsp cert status failed with response status: {ocsp_resp.status_code}')
 
+def get_issuer(cert):
+    aia = cert.extensions.get_extension_for_oid(ExtensionOID.AUTHORITY_INFORMATION_ACCESS).value
+    issuers = [ia for ia in aia if ia.access_method == AuthorityInformationAccessOID.CA_ISSUERS]
+    if not issuers:
+        raise Exception(f'no issuers entry in AIA')
+    return issuers[0].access_location.value
 
-def get_cert_status_for_host(hostname, port):
-    print('   hostname:', hostname, "port:", port)
-    cert = get_cert_for_hostname(hostname, port)
-    ca_issuer = get_issuer(cert)
-    print('   issuer ->', ca_issuer)
+def get_cert_status(cert):  
+    ca_issuer = get_issuer(cert)    
     issuer_cert = get_issuer_cert(ca_issuer)
     ocsp_server = get_ocsp_server(cert)
     print('   ocsp_server ->', ocsp_server)
     return get_ocsp_cert_status(ocsp_server, cert, issuer_cert)
 
-status = get_cert_status_for_host('software7.com', 443)
-print('software7.com:', status, '\n')
+certPEM = open("/etc/pki/ca/issuing_ca/certs", 'r').read()
+cert = x509.load_pem_x509_certificate(certPEM.encode('ascii'), default_backend())
+print(get_cert_status(cert))
