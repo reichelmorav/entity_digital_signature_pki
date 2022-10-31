3d51f9b4bdeed85a3532dfad06b5d26d6c061827
madrigalsalazar <diego.madrigalsalazar@ucr.ac.cr>
2022-10-31 08:40:56 -0600

    crl

--- crl_validator.sh
+++ crl_validator.sh
@@ -2,8 +2,7 @@
 
 rm /etc/pki/crls/ca_intermediate_entities_issuing.crl /etc/pki/crls/issuing.crl
 cert_serial="$(openssl x509 -noout -serial -in $1 | cut -d'=' -f2)"
-crl_URL="$(openssl asn1parse -in $1 | grep -A 1 'X509v3 CRL Distribution Points' | tail -1 | cut -d: -f 4 | cut -b21- 
-| perl -ne 's/(..)/print chr(hex($1))/ge; END {print "\n"}')"
+crl_URL="$(openssl asn1parse -in $1 | grep -A 1 'X509v3 CRL Distribution Points' | tail -1 | cut -d: -f 4 | cut -b21- | perl -ne 's/(..)/print chr(hex($1))/ge; END {print "\n"}')"
 
 wget -P /etc/pki/crls/ "$crl_URL" -q
 
