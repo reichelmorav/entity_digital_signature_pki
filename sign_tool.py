a3ab17f8a76dd1ded90be8b7bac8dea5c493b2f8
madrigalsalazar <diego.madrigalsalazar@ucr.ac.cr>
2022-10-30 20:49:05 -0600

    sign tool

--- sign_tool.py
+++ sign_tool.py
@@ -1,21 +1,24 @@
 from asyncio.log import logger
-import tabulate
+from tabulate import tabulate
 from termcolor import colored
 
 def main():       
     while(True):    
-        table = [['BIENVENIDO(A) A LA APLICACIÓN DE FIRMA DIGITAL DE LA UCR'], ['1. Firmar documento'], ['2. Validar firma'], ['3. Salir']]
-        print(tabulate(table, headers='firstrow', tablefmt='fancy_grid'))
-        option = input("Ingrese el número de la opción deseada: ")
-        if(option == "1"):      
-            break                        
-        elif(option == "2"):            
+        try:                  
+            table = [['BIENVENIDO(A) A LA APLICACIÓN DE FIRMA DIGITAL DE LA UCR'], ['1. Firmar documento'], ['2. Validar firma'], ['3. Salir']]
+            print(tabulate(table, headers='firstrow', tablefmt='fancy_grid'))
+            option = input("Ingrese el número de la opción deseada: ")
+            if(option == "1"):      
+                break                        
+            elif(option == "2"):            
+                break
+            elif(option == "3"):            
+                break
+            else:
+                print("La opción seleccionada es incorrecta. Inténtelo de nuevo.\n")
+        except BaseException as exception:
+            logger.error('Sorry, there is an exception: '+ str(exception))
             break
-        elif(option == "3"):            
-            break
-        else:
-            print("La opción seleccionada es incorrecta. Inténtelo de nuevo.\n")
-   
 
 if __name__ == "__main__":    
     main()
