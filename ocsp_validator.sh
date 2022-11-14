e0913695a1d06cfe6d0eff04871fcb1809dba38e
madrigalsalazar <diego.madrigalsalazar@ucr.ac.cr>
2022-11-13 22:00:50 -0600

    c

--- ocsp_validator.sh
+++ ocsp_validator.sh
@@ -3,3 +3,5 @@
 ocsp_url="$(openssl x509 -noout -ocsp_uri -in $1)"
 
 status="$(openssl ocsp -CAfile /etc/pki/ca_chain_issuing.cert.pem -url "$ocsp_url" -port 8080 -issuer /etc/pki/ca_intermediate_issuing.cert.pem -cert $1)"
+
+echo "$status"
